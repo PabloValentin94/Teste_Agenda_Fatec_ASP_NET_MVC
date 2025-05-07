eb3c048a6d666432060ad8219634b8dc54b93c52
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-05-07 18:00:47 -0300

    Adicionando as páginas independentes do projeto.

--- Teste_Agenda_Fatec/Controllers/HomeController.cs
+++ Teste_Agenda_Fatec/Controllers/HomeController.cs
@@ -1,31 +1,67 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+
 using Teste_Agenda_Fatec.Models;
 
 namespace Teste_Agenda_Fatec.Controllers;
 
 public class HomeController : Controller
 {
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
+
         _logger = logger;
+
     }
+
+    /*
+     
+        /Home ou /Home/Index    
+
+    */
 
     public IActionResult Index()
     {
+
         return View();
+
     }
 
-    public IActionResult Privacy()
+    /*
+     
+        /Home/Equipe    
+
+    */
+
+    public IActionResult Equipe()
     {
+
         return View();
+
+    }
+
+    /*
+     
+        /Home/Ajuda    
+
+    */
+
+    public IActionResult Ajuda()
+    {
+
+        return View();
+
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
+
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    
     }
+
 }
